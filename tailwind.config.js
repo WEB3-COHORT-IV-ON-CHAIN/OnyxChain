6cc0f38f6d92c0eecdc7d17633659b83e7525850
Promise278 <promiseobi2007@gmail.com>
2026-01-30 09:31:13 +0100

    updated input field for user's to create passowrd and also to signin with a password and added a create wallet button below the signin button for a user to be able to create a new wallet

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: ["./src/**/*.{html,js}"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+/** @type {import('tailwindcss').Config} */
+export default {
+  // Add ts and tsx here!
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
